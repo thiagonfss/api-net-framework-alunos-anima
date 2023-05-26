eafe545cc9ba6339dcee4161d31c5a9bf9a22c72
MatheusChavesF <matheus.chaves@animaeducacao.com.br>
2023-05-25 23:04:17 -0300

    Criação DTO's e MODEL's

---
+++ Execricio.NETFramework.CRUD.Business/Models/ProdutoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execricio.NETFramework.CRUD.Business.Models
+{
+    public class ProdutoModel 
+    {
+        public Guid Id_Produto { get; set; }
+        public string Nome_produto { get; set; }
+        public string Descricao_produto { get; set; }
+        public double Preco_atual { get; set; }
+    }
+}
